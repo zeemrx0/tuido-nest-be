b62851608f4a541cf3f60bac11519f9475625b8d
Duy Huynh <48668462+zeemrx0@users.noreply.github.com>
2025-11-22 21:01:17 +0700

    fix: use user repository interface instead (#5)

--- src/modules/user/domain/user.port.ts
+++ src/modules/user/domain/user.port.ts
@@ -1,5 +1,11 @@
-import { UserLoginDto, UserRegistrationDto } from '@user/domain/user.dto';
+import { UserConditionDto, UserLoginDto, UserRegistrationDto, UserUpdateDto } from '@user/domain/user.dto';
 import { User } from '@user/domain/user.model';
+import { IRepository } from '@shared/interfaces';
+
+export const USER_REPOSITORY = Symbol('IUserRepository');
+export const USER_SERVICE = Symbol('IUserService');
+
+export interface IUserRepository extends IRepository<User, UserConditionDto, UserUpdateDto> {}
 
 export interface IUserService {
   register(data: UserRegistrationDto): Promise<string>;
